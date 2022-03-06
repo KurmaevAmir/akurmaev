508002032d8c7b0b358e6c73270a937e27e9ffa8
KurmaevAmir <amirku05@yandex.ru>
2022-03-06 21:58:34 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route("/"):
+def index():
+    return """Добро пожаловать!"""
+
+
+if __name__ == '__main__':
+    serve(app, host="0.0.0.0", port=5000)
